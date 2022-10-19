98e6d637f4d8c4c8fbd52814c31cac0c07d0f2dc
Andhika <andhika.ardhiansyah19@gmail.com>
2022-10-19 08:38:41 +0700

    week2

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	// Import dumbmerch/database here ...
+	// Import dumbmerch/pkg/mysql here ...
+	"dumbmerch/database"
+	"dumbmerch/pkg/mysql"
+	"dumbmerch/routes"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// initial DB here ...
+	mysql.DatabaseInit()
+	// Run migration here ...
+	database.RunMigration()
+
+	r := mux.NewRouter()
+
+	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
+
+	fmt.Println("server running localhost:5000")
+	http.ListenAndServe("localhost:5000", r)
+}
